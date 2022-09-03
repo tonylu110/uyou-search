7eaf33e3933a9170ae0b370e6c62c6623999d3f9
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2022-09-03 11:53:37 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import SearchBar from './components/SearchBar/SearchBar.vue';
+</script>
+
+<template>
+  <SearchBar />
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/SearchBar/SearchBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <input type="text">
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+input {
+  background-color: #00000020;
+  border: none;
+  -webkit-app-region: no-drag;
+  width: 100%;
+  height: 100%;
+  border-radius: 7px;
+  padding: 0 10px;
+  font-size: 16px;
+  outline: none;
+  @include font-color;
+}
+</style>
