212b576d228834aac7f18e7d15e13eaec0750e6b
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2022-09-06 10:12:48 +0800

    Add: search button

--- src/components/SearchBar/SearchBar.vue
+++ src/components/SearchBar/SearchBar.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="search-bar">
+  <div class="search-bar" :style="{borderBottom: broder}">
     <div class="close-button" @click="closeWindow">
       <span class="material-icons">close</span>
     </div>
-    <input type="text" class="input" v-model="text">
+    <div class="input">
+      <input type="text" v-model="text" />
+      <div class="search-button" @click="search" :style="{right: text.length > 0 ? '5px' : ''}">
+        <span class="material-icons">search</span>
+      </div>
+    </div>
     <div class="show">
       <span class="material-icons">close</span>
     </div>
@@ -12,6 +17,8 @@
 
 <script setup lang="ts">
 import { ref, watchEffect } from 'vue';
+
+const { shell } = require('electron')
 
 const ipcRenderer = require('electron').ipcRenderer
 const closeWindow = () => {
@@ -29,75 +36,12 @@ watchEffect(() => {
   ipcRenderer.send('change-big-window', text.value.length > 0)
   broder.value = (text.value.length > 0) ? '2px solid #75757520' : ''
 })
+
+const search = () => {
+  shell.openExternal(`https://www.baidu.com/s?wd=${text.value}`) 
+}
 </script>
 
 <style scoped lang="scss">
-@import '../../color.scss';
-
-.search-bar {
-  -webkit-app-region: drag;
-  width: calc(100vw - 30px);
-  height: 50px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 15px;
-  overflow: hidden;
-  border-bottom: v-bind(broder);
-
-  input {
-    background-color: #75757520;
-    border: none;
-    -webkit-app-region: no-drag;
-    width: 100%;
-    height: 100%;
-    border-radius: 7px;
-    padding: 0 10px;
-    font-size: 16px;
-    outline: none;
-    transition: 0.3s width;
-    @include font-color;
-  }
-
-  .close-button {
-    -webkit-app-region: no-drag;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 5px;
-    border-radius: 50%;
-    color: transparent;
-    position: fixed;
-    right: 15px;
-    z-index: 1;
-    cursor: pointer;
-
-    &:hover + .input {
-      width: calc(100% - 65px);
-    }
-
-    &:active {
-      background-color: #00000020;
-    }
-
-    &:hover ~ .show {
-      opacity: 1;
-      right: 15px;
-    }
-  }
-
-  .show {
-    opacity: 0;
-    position: fixed;
-    transition: 0.3s;
-    right: -30px;
-    padding: 5px;
-    border-radius: 50%;
-    @include font-color;
-    background-color: #75757520;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-}
+@import './style.scss';
 </style>
